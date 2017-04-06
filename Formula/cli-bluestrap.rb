06a5e8a7cb2aab7cc0c3608cc3cd693f5a6e06ef
Ben White <hello@bemm.com.au>
2017-04-07 02:18:44 +1000

    Update cli-bluestrap.rb

--- Formula/cli-bluestrap.rb
+++ Formula/cli-bluestrap.rb
@@ -2,7 +2,7 @@ class CliBluestrap < Formula
   desc 'cli-bluestrap'
   homepage 'https://bitbucket.org/ffxblue/cli-bluestrap'
   url 'https://s3-ap-southeast-2.amazonaws.com/playpen-homebrew/cli-bluestrap-0.0.1.tar.gz'
-  sha256 '05927d6cd3fddf6b081aa5dd86a71af6e8e9b4ec145da368c45e5e4e0c2ce5d7'
+  sha256 '82237835efb286813c0397b16dea7491d3371b0262c25f792077f96d585b33e3'
   version '0.0.1'
 
   conflicts_with "pharod", :because => "it is broken"
