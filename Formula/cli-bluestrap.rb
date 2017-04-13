03e23c212fcb33490c17ea25bb86a5436f36dc1a
Concourse CI <concourse@concourse>
2017-04-13 07:49:20 +0000

    Updated cli-bluestrap.rb

--- Formula/cli-bluestrap.rb
+++ Formula/cli-bluestrap.rb
@@ -1,9 +1,9 @@
 class CliBluestrap < Formula
   desc 'cli-bluestrap'
   homepage 'https://bitbucket.org/ffxblue/cli-bluestrap'
-  url 'https://s3-ap-southeast-2.amazonaws.com/infrastructure-clibluestrap-development-v1/cli-bluestrap-0.11.0.tar.gz'
-  sha256 '79ed80e783dae004d81308524bcb5ab12d08f3097318e5da6db3de997438f64e'
-  version '0.11.0'
+  url 'https://s3-ap-southeast-2.amazonaws.com/infrastructure-clibluestrap-development-v1/cli-bluestrap-0.12.0.tar.gz'
+  sha256 'cb12cfa541523f1e6b4a7a1955a0ddc94c09547f3152263e60fa6be2a55d6784'
+  version '0.12.0'
 
   conflicts_with "pharod", :because => "it is broken"
 
