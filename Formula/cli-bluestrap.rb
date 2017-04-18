afb7154f0ad510ca08c19e75ac999459ed11a97a
Concourse CI <concourse@concourse>
2017-04-18 03:47:34 +0000

    Updated cli-bluestrap.rb

--- Formula/cli-bluestrap.rb
+++ Formula/cli-bluestrap.rb
@@ -1,16 +1,16 @@
 class CliBluestrap < Formula
   desc 'cli-bluestrap'
   homepage 'https://bitbucket.org/ffxblue/cli-bluestrap'
-  url 'https://s3-ap-southeast-2.amazonaws.com/infrastructure-clibluestrap-development-v1/cli-bluestrap-0.16.0.tar.gz'
-  sha256 'abb4d63d6c2e76fa96eee85d0d3402df09e5fda8fa91e10f8836e81c96575060'
-  version '0.16.0'
+  url 'https://s3-ap-southeast-2.amazonaws.com/infrastructure-clibluestrap-development-v1/cli-bluestrap-0.17.0.tar.gz'
+  sha256 'd8257f4e3945709fae137230e960b32094de9546f5d6fe3236d9731cd7d0161c'
+  version '0.17.0'
 
   conflicts_with "pharod", :because => "it is broken"
 
   def install
     ENV.prepend_path "PATH", "/usr/local/bin"
     ENV["PREFIX"] = prefix
-    system 'make', 'install', 'VERSION=0.16.0'
+    system 'make', 'install', 'VERSION=0.17.0'
   end
 
   test do
