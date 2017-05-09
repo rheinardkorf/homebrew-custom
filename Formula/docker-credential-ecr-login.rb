5d213862b419a3b9e3b233b4b7632522c0d8b451
Michael Lorant <michael.lorant@fairfaxmedia.com.au>
2017-05-09 15:44:24 +1000

    Updated ecr helper

--- Formula/docker-credential-ecr-login.rb
+++ Formula/docker-credential-ecr-login.rb
@@ -2,13 +2,13 @@ require 'formula'
 require 'tmpdir'
 
 class DockerCredentialEcrLogin < Formula
-  VERSION = '589f7070767646f2b220504930f803accb1e88a7'
+  VERSION = '19c5038b3c1fc63328b4eb99c507c72f12a27f66'
 
   homepage 'https://github.com/awslabs/amazon-ecr-credential-helper'
   url "https://github.com/awslabs/amazon-ecr-credential-helper/archive/#{VERSION}.tar.gz"
-  sha256 '99947939b9997a17612db0634f13fcfb769e96dd549b1964a8e2d5d5e573661b'
+  sha256 'f5bcc481812942d3314e38c2406b881c74ad0dabcb69707a0f7c55cdc95d1828'
   version VERSION
-  head 'https://github.com/awslabs/amazon-ecr-credential-helper.git', :branch => 'implement-helper-list'
+  head 'https://github.com/awslabs/amazon-ecr-credential-helper.git', :branch => 'master'
   depends_on 'go' => :build
 
   def install
