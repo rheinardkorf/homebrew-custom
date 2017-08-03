db66e469507f667c5e58a9cc00f8f52f4802407c
Concourse CI <concourse@concourse>
2017-08-03 02:07:48 +0000

    Updated cli-bluestrap.rb

--- Formula/cli-bluestrap.rb
+++ Formula/cli-bluestrap.rb
@@ -1,14 +1,14 @@
 class CliBluestrap < Formula
   desc 'cli-bluestrap'
   homepage 'https://bitbucket.org/ffxblue/cli-bluestrap'
-  url 'https://s3-ap-southeast-2.amazonaws.com/cli-bluestrap.ffxblue.com.au/cli-bluestrap-0.60.0.tar.gz'
-  sha256 'fd18ef30906a1ac1184c1b4479a405c8303261818949183720cea0747fc48f4a'
-  version '0.60.0'
+  url 'https://s3-ap-southeast-2.amazonaws.com/cli-bluestrap.ffxblue.com.au/cli-bluestrap-0.61.0.tar.gz'
+  sha256 '26aad4851704f45d5581cfefb647bbee29c849f1c28c4f88b5e1e0d7144b8846'
+  version '0.61.0'
 
   def install
     ENV.prepend_path "PATH", "/usr/local/bin"
     ENV["PREFIX"] = prefix
-    system 'make', 'install', 'VERSION=0.60.0'
+    system 'make', 'install', 'VERSION=0.61.0'
   end
 
   test do
