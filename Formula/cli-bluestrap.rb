c3b070f3fb0bf5c6f20cb5133e41f57bf5762137
Concourse CI <concourse@concourse>
2017-08-09 21:18:16 +0000

    Updated cli-bluestrap.rb

--- Formula/cli-bluestrap.rb
+++ Formula/cli-bluestrap.rb
@@ -1,14 +1,64 @@
 class CliBluestrap < Formula
   desc 'cli-bluestrap'
   homepage 'https://bitbucket.org/ffxblue/cli-bluestrap'
-  url 'https://s3-ap-southeast-2.amazonaws.com/cli-bluestrap.ffxblue.com.au/cli-bluestrap-0.61.0.tar.gz'
-  sha256 '26aad4851704f45d5581cfefb647bbee29c849f1c28c4f88b5e1e0d7144b8846'
-  version '0.61.0'
+  url 'https://s3-ap-southeast-2.amazonaws.com/cli-bluestrap.ffxblue.com.au/cli-bluestrap-0.62.0.tar.gz'
+  sha256 '14aceb71420a30159a45b7c6362c028016d01af8774dbe861e8ca6fe72f32053'
+  version '0.62.0'
+
+  # General
+  depends_on 'awscli'
+  depends_on 'git'
+  depends_on 'jq'
+  depends_on 'hostess'
+  depends_on 'ffxblue/custom/docker-credential-ecr-login'
+
+  # Backend
+  depends_on 'go'
+  depends_on 'goenv'
+  depends_on 'glide'
+  depends_on 'openssl'    # api-auth
+  depends_on 'libxmlsec1' # api-auth
+  depends_on 'protobuf'   # gRPC API servers/clients
+
+  # Frontend
+  depends_on 'node'
+  depends_on 'nodenv'
+  depends_on 'yarn'
+  depends_on 'watchman'
 
   def install
+    # Use 'docker' instead of '/usr/local/bin/docker'
     ENV.prepend_path "PATH", "/usr/local/bin"
-    ENV["PREFIX"] = prefix
-    system 'make', 'install', 'VERSION=0.61.0'
+    # For ecr-helper
+    ENV["AWS_SDK_LOAD_CONFIG"] = "true"
+    # For symlinking from user home into homebrew 'superenv'
+    localuser = ENV["USER"]
+
+    # Install casks (can't use 'depends_on' yet)
+    system "brew cask install bitbar"
+    system "brew cask install docker"
+    system "brew cask install kitematic"
+
+    # Check Docker is good to go
+    system "bin/check"
+
+    # Get cli-bluestrap image
+    ln_s "/Users/#{localuser}/.aws", "#{buildpath}/.brew_home/.aws"
+    ln_s "/Users/#{localuser}/.docker", "#{buildpath}/.brew_home/.docker"
+    # Debugging
+    # system "/bin/cat", "#{buildpath}/.brew_home/.aws/credentials"
+    # system "/bin/cat", "#{buildpath}/.brew_home/.aws/config"
+    # system "/bin/cat", "#{buildpath}/.brew_home/.docker/config.json"
+    # system "/bin/bash", "-c", "/usr/local/bin/docker-credential-ecr-login list"
+    system "docker", "pull", "175914186171.dkr.ecr.ap-southeast-2.amazonaws.com/tools/cli-bluestrap:#{version}"
+
+    # Install Bitbar plugin
+    share.install Dir["bin/bitbar-*"]
+
+    # Switch version and install bs
+    inreplace "bs-wrapper", ":latest", ":#{version}"
+    mv "bs-wrapper", "bs"
+    bin.install "bs"
   end
 
   test do
